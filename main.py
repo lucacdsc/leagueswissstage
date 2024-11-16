cad48dff2445561bdac7ffd4e5e1eef965e5b035
Luca Clemente <clementeluca@proton.me>
2024-11-16 19:34:39 -0300

    Some analyses of the League of Legends dataset

--- main.py
+++ main.py
@@ -2,6 +2,51 @@
 import pandas as pd
 # %%
 df = pd.read_csv('data/champions.csv')
-df
+df.head(50)
 # %%
-df
+df.info()
+df.describe()
+# %%
+champions = df['Champion'].values
+
+for champion in champions:
+  print(f'\n Champion: {champion}')
+
+# %%
+# Filtre os campeões com mais de 50% de Presence.
+df['Presence'] = df['Presence'].str.replace('%','').astype(float)
+
+champions_over_50_percent_presence = df['Presence'] > 50
+df[champions_over_50_percent_presence]
+
+# %%
+# Ordenar os campeões por número de Picks do maior para o menor.
+df.info()
+champions_most_pick = df.sort_values(by='Picks', ascending=False)
+champions_most_pick
+
+# %%
+#Filtre os campeões com Winrate maior que 60%.
+df['Winrate'] = df['Winrate'].str.replace('%','').astype(float)
+
+champions_over_60_percent_winrate = df['Winrate'] > 60
+df[champions_over_60_percent_winrate]
+# %%
+#Liste os 5 campeões com a maior KDA.
+df.info()
+df['KDA'].dtype
+df['KDA'] = pd.to_numeric(df['KDA'], errors='coerce')
+df['KDA'].dtype
+
+df.nlargest(5,'KDA')
+
+# %%
+#Quais são os campeões com os maiores valores de DPM?
+df['DPM'].dtype
+df.nlargest(3,'DPM')
+# %%
+# Liste os campeões com GD@15 positivo (ganhando mais ouro aos 15 minutos do que seus adversários).
+df.info()
+champions_winning_lane_phase = df['GD@15'] > 0
+df[champions_winning_lane_phase].sort_values(by='GD@15',ascending=False).head()
+# %%
