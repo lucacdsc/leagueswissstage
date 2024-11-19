6dcf71f10661a6d42d060bca3a625e935c43aa7b
Luca Clemente <clementeluca@proton.me>
2024-11-19 15:13:38 -0300

    plot with matplotlib

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 numbers = [1,7,2]
 
@@ -15,3 +16,8 @@ my_dataframe = pd.DataFrame(data,index=['day1','day2','day3'])
 print(my_dataframe)
 
 print(my_dataframe.loc['day1'])
+
+exercise = pd.read_csv('./data/data.csv')
+
+exercise.plot(kind='scatter', x='Duration', y='Calories')
+plt.show()
